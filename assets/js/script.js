4432b3dff6ef21f261c74125ac95bd454064852c
Sowmya Ankamahadeva <sowmya.ack@gmail.com>
2025-01-26 22:41:11 +0000

    made changes as per mentor call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 const quote= document.getElementById("quote");
 const author= document.getElementById("author");
-const api_url= "https://api.quotable.io/random";
+const api_url= "http://api.quotable.io/random";
 
 async function getquote(url){
 
-    const response=await fetch(url);
-    var data =await response.json();
-    //console.log(data);
-    quote.innerHTML = data.content;
-    author.innerHTML = data.author;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        quote.style.color = "black";
+        quote.innerHTML = data.content;
+        author.innerHTML =data.author;
+    }).catch(() => {
+        quote.innerHTML = "A problem occured, please try again later.";
+        quote.style.color ="red";
+        author.innerHTML = "The Devs";
+    });
 }
+
 
 getquote(api_url);
