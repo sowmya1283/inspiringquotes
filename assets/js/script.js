43f353d3fa6fd80ddc3ea687d45eb926fff15190
Sowmya Ankamahadeva <sowmya.ack@gmail.com>
2025-01-26 22:42:12 +0000

    changed url to test, working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const quote= document.getElementById("quote");
 const author= document.getElementById("author");
-const api_url= "http://api.quotable.io/random";
+const api_url= "https://api.quotable.io/random";
 
 async function getquote(url){
 
