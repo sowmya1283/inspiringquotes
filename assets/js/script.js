b922a0e5169c7c3dd821ddd3b40d28905e2845d5
Sowmya Ankamahadeva <sowmya.ack@gmail.com>
2025-01-28 17:40:28 +0000

    added the quote.innerhtml to post on twitter on clicking tweet button

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,5 +21,5 @@ async function getquote(url) {
 getquote(api_url);
 
 function tweet(){
-window.open("https://twitter.com/intent/tweet?text=Hello%20world", "Tweet Window", "width=600 height=300")
+window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML, "Tweet Window", "width=600 height=300")
 }
